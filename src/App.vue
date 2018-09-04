bfa933462c13554a2b3104b05e3121564e794390
Bas Horselenberg <bashorselenberg@gmail.com>
2018-09-04 07:09:17 +0200

    basic css setup. basic nav and home.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+      <custom-nav />
     <router-view/>
   </div>
 </template>
 
 <script>
+    import customNav from '@/components/customNav'
 export default {
-  name: 'App'
+  data() {
+      return {
+
+      }
+  },
+    components: {
+      customNav
+    }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import homepage from '@/components/homepage'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      {
+          path: '/',
+          name: 'Home',
+          component: homepage
+      }
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
   ]
 })
