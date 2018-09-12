c3ddae60cf329b610adf41ad024311374d5218d0
akammaru <bashorselenberg@gmail.com>
2018-09-12 16:27:20 +0200

    setup store/multi language

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,24 @@
 
 <script>
     import customNav from '@/components/customNav'
+    import ENV from '../env'
+
 export default {
   data() {
       return {
 
       }
   },
-    components: {
-      customNav
+  components: {
+    customNav
+  },
+  beforeCreate() {
+    let tansltions_fetched = this.$store.getters.getTranslations
+    if(ENV.debug_mode) {
+      console.log('debug mode is active, please disable for production.')
+      console.log("translations have been fetched: " + tansltions_fetched)
     }
+  }
 
 }
 </script>
